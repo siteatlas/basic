ec86cefeb3b8dfd8ea949545fa041922ce3998fb
Wirote <Wirote@WiroteInfoRBH2>
2016-04-28 15:08:35 +0700

    อบรมวันที่ 27 เม.ย.59

--- controllers/KpiController.php
+++ controllers/KpiController.php
@@ -39,4 +39,35 @@ where d.byear = 2559";
         ]);
     }
 
+    public function actionKpi3() {
+$sql = "select k.kpiname, k.acol, k.bcol, k.target,
+d.divide, d.denom, d.byear,
+d.divide*100/d.denom as result
+from kpi k join kpidata d on k.id = d.kpiid
+where d.byear = 2559";
+        $data = \Yii::$app->db->createCommand($sql)
+                ->queryAll();
+        $dataProvider = new ArrayDataProvider([
+            'allModels' => $data
+        ]);
+        return $this->render('kpi3',[
+            'dataProvider'=>$dataProvider
+        ]);
+    }
+    
+    public function actionKpi4() {
+$sql = "select k.kpiname, k.acol, k.bcol, k.target,
+d.divide, d.denom, d.byear,
+d.divide*100/d.denom as result
+from kpi k join kpidata d on k.id = d.kpiid
+where d.byear = 2559";
+        $data = \Yii::$app->db->createCommand($sql)
+                ->queryAll();
+        $dataProvider = new ArrayDataProvider([
+            'allModels' => $data
+        ]);
+        return $this->render('kpi4',[
+            'dataProvider'=>$dataProvider
+        ]);
+    }
 }
---
+++ views/kpi/kpi3.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\grid\GridView;
+$this->title = 'KPI3';
+$this->params['breadcrumbs'][] = [
+    'label' => 'KPI Menu',
+    'url' => [
+        '/kpi/index',
+    ]
+];
+$this->params['breadcrumbs'][] = $this->title;
+
+
+?>
+<?= GridView::widget([
+    'dataProvider'=>$dataProvider,
+    'tableOptions' => [
+        'class' => 'table 
+            table-striped 
+            table-bordered 
+            table-responsive 
+            table-hover'
+    ],
+    'headerRowOptions'=>['class'=>'danger'],
+    'columns'=>[
+        [
+            'label'=>'ตัวชี้วัด',
+            'attribute'=>'kpiname',
+            'headerOptions'=>['class'=>'text-center'],
+        ],
+        [
+            'label'=>'ตัวตั้ง',
+            'attribute'=>'divide',
+            'headerOptions'=>['class'=>'text-center'],
+            'contentOptions'=>['class'=>'text-right'],
+            'format'=>['decimal',0],
+        ],
+        [
+            'label'=>'ตัวหาร',
+            'attribute'=>'denom',
+            'headerOptions'=>['class'=>'text-center'],
+            'contentOptions'=>['class'=>'text-right'],
+            'format'=>['decimal',0],
+        ],
+        [
+            'label'=>'ผลลัพธ์',
+            'attribute'=>'result',
+            'headerOptions'=>['class'=>'text-center'],
+            'contentOptions'=>['class'=>'text-right'],
+            'format'=>['decimal',2],
+        ],
+        [
+            'label'=>'เป้าหมาย',
+            'attribute'=>'target',
+            'headerOptions'=>['class'=>'text-center'],
+            'contentOptions'=>['class'=>'text-right'],
+            'format'=>['decimal',2],
+        ],
+    ]
+]);
+?>
